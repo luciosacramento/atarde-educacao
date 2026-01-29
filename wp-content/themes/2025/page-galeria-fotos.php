c172854b08faba4697283c96f533278ed6021617
lucio.sacramento <lucio.sacramento@tcm.ba.gov.br>
2026-01-29 16:50:31 -0300

    feat: Add photo gallery templates, styles, and initial image uploads.

--- wp-content/themes/2025/page-galeria-fotos.php
+++ wp-content/themes/2025/page-galeria-fotos.php
@@ -9,86 +9,87 @@
  * @since A tarde Educação 1.0
  */
 
- get_header();
+get_header();
 
- wp_reset_query();
+wp_reset_query();
 
- ?>
+?>
 
- <!-- Conteúdo -->
- <section class="py-5">
+<!-- Conteúdo -->
+<section class="py-5">
     <div class="container">
-      <div class="row mt-4" id="noticias-container">
+        <div class="row mt-4" id="noticias-container">
 
 
-        <?php
-                // Configuração da consulta para buscar os posts do tipo 'post'
-                $args = array(
-                    'post_type'      => 'galeria_fotos',      // Tipo de conteúdo 'post' (padrão para notícias)
-                    'posts_per_page' => 18,         // Quantidade de posts (notícias)
-                    'post_status'    => 'publish',  // Apenas posts publicados
-                    'orderby'        => 'date',     // Ordenar por data
-                    'order'          => 'DESC',     // Ordem decrescente (mais recentes primeiro)
-                );
+            <?php
+            // Configuração da consulta para buscar os posts do tipo 'post'
+            $args = array(
+                'post_type' => 'galeria_fotos',      // Tipo de conteúdo 'post' (padrão para notícias)
+                'posts_per_page' => 18,         // Quantidade de posts (notícias)
+                'post_status' => 'publish',  // Apenas posts publicados
+                'orderby' => 'date',     // Ordenar por data
+                'order' => 'DESC',     // Ordem decrescente (mais recentes primeiro)
+            );
 
-                // Executa a consulta
-                $query = new WP_Query($args);
+            // Executa a consulta
+            $query = new WP_Query($args);
 
-                // Verifica se existem posts
-                if ($query->have_posts()) {
-                    $noticias = array();
+            // Verifica se existem posts
+            if ($query->have_posts()) {
+                $noticias = array();
 
-                    while ($query->have_posts()) {
-                        $query->the_post(); // Configura o post atual
-
-                        $imagem_destaque = get_the_post_thumbnail_url(get_the_ID(), 'noticias-list'); // URL da imagem em destaque (tamanho completo)
-                        if (!$imagem_destaque) {
-                            $imagem_destaque = 'https://placehold.co/600x400/png'; // URL padrão (imagem de placeholder)
-                        }
-
-                        //$media_link = get_post_meta(get_the_ID(), 'media_link', true);
-
-                        ?>
-
-                        
-                            <div class="col-md-4 p-3">
-                            <div class="card">
-                                <a target="_blank"  href="<?php echo get_the_permalink() ?>" >
-                                    <img src="<?php echo $imagem_destaque;?>" class="card-img-top" alt="<?php echo get_the_title(); ?>">
-                                </a>   
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo get_the_title(); ?></h5>
-                                </div>                             
-                            </div>
-                            </div>
-
-                        <?php
-
-                        // Monta o array com as informações da notícia
-                        $noticias[] = array(
-                            'titulo'     => get_the_title(),                            // Título da notícia
-                            'resumo'     => get_the_excerpt(),                         // Resumo da notícia
-                            'data'       => get_the_date('d/m/Y'),                     // Data formatada no padrão dd/mm/yyyy
-                            'link'       => get_permalink(),                           // Link da notícia
-                        );
+                while ($query->have_posts()) {
+                    $query->the_post(); // Configura o post atual
+            
+                    $imagem_destaque = get_the_post_thumbnail_url(get_the_ID(), 'noticias-list'); // URL da imagem em destaque (tamanho completo)
+                    if (!$imagem_destaque) {
+                        $imagem_destaque = 'https://placehold.co/600x400/png'; // URL padrão (imagem de placeholder)
                     }
 
-                    // Reseta os dados da consulta
-                    wp_reset_postdata();
+                    //$media_link = get_post_meta(get_the_ID(), 'media_link', true);
+            
+                    ?>
 
-                } else {
-                    return 'Sem notícias no momento'; // Nenhuma notícia encontrada
+
+                    <div class="col-md-4 p-3">
+                        <div class="card">
+                            <a target="_blank" href="<?php echo get_the_permalink() ?>">
+                                <img src="<?php echo $imagem_destaque; ?>" class="card-img-top"
+                                    alt="<?php echo get_the_title(); ?>">
+                            </a>
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo get_the_title(); ?></h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+
+                    // Monta o array com as informações da notícia
+                    $noticias[] = array(
+                        'titulo' => get_the_title(),                            // Título da notícia
+                        'resumo' => get_the_excerpt(),                         // Resumo da notícia
+                        'data' => get_the_date('d/m/Y'),                     // Data formatada no padrão dd/mm/yyyy
+                        'link' => get_permalink(),                           // Link da notícia
+                    );
                 }
-        ?>
 
-      </div>
-      <button id="mais-noticias" data-paged="1" tipo="galeria_fotos" >Carregar mais</button>
-      <script>
-         var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
-      </script>
-      
+                // Reseta os dados da consulta
+                wp_reset_postdata();
+
+            } else {
+                return 'Sem notícias no momento'; // Nenhuma notícia encontrada
+            }
+            ?>
+
+        </div>
+        <button id="mais-noticias" data-paged="1" tipo="galeria_fotos">Carregar mais</button>
+        <script>
+            var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+        </script>
+
     </div>
-  </section>
+</section>
 
 <div id="modal" style="display: none;">
     <div id="modal-content">
@@ -98,7 +99,7 @@
         </div>
     </div>
 </div>
-  
 
-  <!-- Fim Conteúdo -->
+
+<!-- Fim Conteúdo -->
 <?php get_footer(); ?>
--- wp-content/themes/2025/single-galeria_fotos.php
+++ wp-content/themes/2025/single-galeria_fotos.php
@@ -9,67 +9,70 @@
  * @since A tarde Educação 1.0
  */
 
- get_header();
+get_header();
 
- wp_reset_query();
+wp_reset_query();
 
- ?>
+?>
 
- <!-- Conteúdo -->
- <section class="py-5">
+
+<!-- Conteúdo -->
+<section class="py-5">
     <div class="container">
-      <div class="row mt-4" id="noticias-container">
+        <div class="row mt-4" id="noticias-container">
 
-            <?php                     
-                $imagens = get_post_meta($post->ID, '_galeria_imagens', true);
-                $imagens = is_array($imagens) ? $imagens : []; // Garante que é um array
-                // Verifica se existem posts
-                if (!empty($imagens)) {
+            <?php
+            $imagens = get_post_meta($post->ID, '_galeria_imagens', true);
+            $imagens = is_array($imagens) ? $imagens : []; // Garante que é um array
+            // Verifica se existem posts
+            if (!empty($imagens)) {
 
-                    foreach ($imagens as $imagem_id) {
+                foreach ($imagens as $imagem_id) {
 
-                        $imagem_destaque = wp_get_attachment_image_url($imagem_id, 'noticias-list'); // URL da imagem em destaque (tamanho completo)
-                        $imagem_destaque_full = wp_get_attachment_image_url($imagem_id, 'full');
-                        $title = get_the_title($imagem_id);
+                    $imagem_destaque = wp_get_attachment_image_url($imagem_id, 'noticias-list'); // URL da imagem em destaque (tamanho completo)
+                    $imagem_destaque_full = wp_get_attachment_image_url($imagem_id, 'full');
+                    $title = get_the_title($imagem_id);
 
-                        if (!$imagem_destaque) {
-                            $imagem_destaque = 'https://placehold.co/600x400/png'; // URL padrão (imagem de placeholder)
-                        }
-
-                        //$media_link = get_post_meta(get_the_ID(), 'media_link', true);
-
-                        ?>
-                            <div class="col-md-4 p-3">
-                            <div class="card">
-                                <a  href="#" class="abrir-modal" data-url="<?php echo esc_url($imagem_destaque_full); ?>" data-title="<?php echo  $title; ?>" data-content="<?php echo get_the_content(); ?>" >
-                                    <img src="<?php echo esc_url($imagem_destaque);?>" class="card-img-top" alt="<?php echo  $title; ?>">
-                                </a>
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo  $title; ?></h5>
-                                </div>                                   
-                            </div>
-                            </div>
-
-                        <?php
-
-                   
+                    if (!$imagem_destaque) {
+                        $imagem_destaque = 'https://placehold.co/600x400/png'; // URL padrão (imagem de placeholder)
                     }
 
-                    // Reseta os dados da consulta
-                    wp_reset_postdata();
+                    //$media_link = get_post_meta(get_the_ID(), 'media_link', true);
+            
+                    ?>
+                    <div class="col-md-4 p-3">
+                        <div class="card">
+                            <a href="#" class="abrir-modal" data-url="<?php echo esc_url($imagem_destaque_full); ?>"
+                                data-title="<?php echo $title; ?>" data-content="<?php echo get_the_content(); ?>">
+                                <img src="<?php echo esc_url($imagem_destaque); ?>" class="card-img-top"
+                                    alt="<?php echo $title; ?>">
+                            </a>
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $title; ?></h5>
+                            </div>
+                        </div>
+                    </div>
 
-                } else {
-                    return 'Sem fotos no momento'; // Nenhuma notícia encontrada
+                    <?php
+
+
                 }
-        ?>
 
-      </div>
-      <script>
-         var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
-      </script>
-      
+                // Reseta os dados da consulta
+                wp_reset_postdata();
+
+            } else {
+                return 'Sem fotos no momento'; // Nenhuma notícia encontrada
+            }
+            ?>
+
+        </div>
+        <script>
+            var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+        </script>
+
     </div>
-  </section>
+</section>
 
 <div id="modal" style="display: none">
     <div id="modal-content" style="width:60%">
@@ -81,7 +84,7 @@
         </div>
     </div>
 </div>
-  
 
-  <!-- Fim Conteúdo -->
+
+<!-- Fim Conteúdo -->
 <?php get_footer(); ?>
